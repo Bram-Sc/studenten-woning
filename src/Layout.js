f0ed4c180d153e9017e5841af485d6ea940cddd1
Bram-Sc <workface@live.nl>
2020-02-19 11:52:58 +0100

    Improved navbar

--- src/Layout.js
+++ src/Layout.js
@@ -5,9 +5,9 @@ function Layout() {
     return (
         <div className="navbar">
             <ul>
-                <li><h3>Title</h3></li>
-                <li><a href="item">Item 1</a></li>
-                <li><a href="item">Item 2</a></li>
+                <li><a className="title" href="#home">Studenten woninginen</a></li>
+                <li><a href="#item">Item 1</a></li>
+                <li><a href="#item">Item 2</a></li>
             </ul>
         </div>
     );
