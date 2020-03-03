c986bc2acbb5fe725c075f6e4e7e5465c06f584a
Bram-Sc <workface@live.nl>
2020-03-03 10:01:56 +0100

    Changed color pallet

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import Item from './Item';
 import Layout from './Layout';
 
 function App() {
-  const testMsg = () => {
-    console.log("Yes");
-  }
-
   return (
     <body>
       <Layout />
-      <button onClick={testMsg}>test</button>
       <div className="flex">
         <Item name="Greg" message="first"/>
         <Item name="Steve" message="second"/>
