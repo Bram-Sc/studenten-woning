cb83d0549e6f5d94c0fab2cc729846b2c6c2e559
Bram-Sc <workface@live.nl>
2020-05-17 12:57:19 +0200

    Changed the navbar and added a router

---
+++ src/Components/Navbar.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { createStyles, makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles(
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    menuButton: {
+      marginRight: 2,
+    },
+    title: {
+      flexGrow: 1,
+      color: "White",
+    },
+    NavBar: {
+      backgroundColor: "#806c52",
+      color: "White",
+      fontFamily: "Comic Sans MS",
+    },
+  })
+);
+
+export default function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.NavBar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <Button href="/index" color="inherit">
+              Studenten Woning
+            </Button>
+            <Button href="/addProduct" color="inherit">
+              Add product
+            </Button>
+            <Button href="/login" color="inherit">
+              Log in
+            </Button>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
